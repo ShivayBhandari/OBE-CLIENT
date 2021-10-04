e32f24e9bbdfa3e116c73d06ecd9c3fc01e9b7e5
Gurlivleen Singh Kainth <gurlivleen.kainth2000@gmail.com>
2021-10-04 23:33:53 +0530

    Course Form Created

--- src/app/pages/courses/courses.component.ts
+++ src/app/pages/courses/courses.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-courses',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoursesComponent implements OnInit {
 
-  constructor() { }
+  loader: boolean = false;
+
+  constructor(
+    private modalService: NgbModal
+  ) { }
 
   ngOnInit(): void {
   }
 
+  openCourseModal(modalRef: any): void {
+    this.modalService.open(modalRef, {
+      size: 'lg'
+    });
+  }
 }
